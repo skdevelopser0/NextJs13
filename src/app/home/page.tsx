a655c99956743d6b4968bdf0d3e0913252dd1565
sanjeev kumar <kumarsanjeev2211@gmail.com>
2023-07-22 14:51:32 +0530

    NextJS 13 with WebAPI in Dotnet Core

---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Router from 'next/router'
+
+export default function page() {
+
+    return (
+        <>
+            <div>This is my home page</div>
+        </>
+    )
+}
